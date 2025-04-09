3b0052dc18a3c382f7198db58c66b7dcbbb274a4
FAIZ ALAM <87482396+FaizAlam4@users.noreply.github.com>
2025-04-10 02:09:38 +0530

    Update EduCert.js

--- src/EduCert.js
+++ src/EduCert.js
@@ -10,16 +10,17 @@ const EduCert = () => {
           <h3>Bachelor of Technology in Computer Science Engineering</h3>
           <p>Lovely Professional University,   <strong> CGPA: 9.05 </strong> [2020-2024]</p> <br />
           <h3>Higher Secondary(12 <sup>th</sup>), Central Board of Secondary Education </h3>
-          <p>Kendriya Vidyalaya No. 1, <strong>Percentage: 91.6%</strong> [2017] </p>
+          <p>Kendriya Vidyalaya No. 1, <strong>Percentage: 91.6%</strong>  </p>
         </div>
         <br />
         <div className="education-item">
-          <h2>Certificates/Certifications <a id='lk' href="https://drive.google.com/drive/folders/1lepuDQLsZsN3qQQ6tm1knUtB7fWtzdCc?usp=sharing" target={"_blank"}>link</a></h2>
+          <h2>Certificates/Certifications <a id='lk' href="https://drive.google.com/drive/folders/1lepuDQLsZsN3qQQ6tm1knUtB7fWtzdCc?usp=sharing" target={"_blank"}>Go</a></h2>
 
+          <p>Node JS (CipherSchool)</p>
+          <p>Database Management Essential (Coursera)</p>
           <p>DevOps Certification (CipherSchool)</p>
           <p>DSA Self-paced (GFG)</p>
           <p>Github master course (Udemy)</p>
-          <p>Database Management Essential (Coursera)</p>
 
         </div>
       </div>
